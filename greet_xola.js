22037f2439a51eb99934d3a763035cadde91391d
MHJOJA <dmenzi694@gmail.com>
2020-08-11 12:14:10 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,2 +1,16 @@
-const greet = require("./greet")
-console.log(greet("Xola"))
+const chalk = require("chalk")
+const greet = require('./greet')
+
+
+var figlet = require('figlet');
+ 
+figlet(greet("Xola"), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    const styledMessage = chalk.bgGreen.black(data)
+    // console.log(data)
+    console.log(styledMessage)
+});
